7049f5e84c3b5f062851a8280309747dd1582190
Arshiya Anjum <143496707+Arshiyanjum@users.noreply.github.com>
2026-01-28 10:48:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var randomNumber1 = Math.floor(Math.random()*6);
-document.querySelectorAll("img")[0].setAttribute('src','./images/dice'+randomNumber1+'.png');
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage1 = "dice"+randomNumber1+".png";
+var randomImageSource1 = "images/"+randomDiceImage1;
+document.querySelectorAll("img")[0].setAttribute('src',randomImageSource1);
 
-var randomNumber2 = Math.floor(Math.random()*6);
-document.querySelectorAll("img")[1].setAttribute('src','./images/dice'+randomNumber2+'.png');
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice"+randomNumber2+".png";
+var randomImageSource2 = "images/"+randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute('src',randomImageSource2);
 
 if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML='Player 1 Wins<img src="images/trophy.gif" style="vertical-align:middle; margin-left:10px;width:80px;">';
+    document.querySelector("h1").innerHTML='Player 1 Wins <img src="images/trophy.gif" style="vertical-align:middle; margin-left:10px;width:80px;">';
 }else if(randomNumber1<randomNumber2){
-        document.querySelector("h1").innerHTML='Player 2 Wins<img src="images/trophy.gif" style="vertical-align:middle; margin-left:10px;width:80px;">';
+        document.querySelector("h1").innerHTML='Player 2 Wins <img src="images/trophy.gif" style="vertical-align:middle; margin-left:10px;width:80px;">';
 
 }else if(randomNumber1===randomNumber2){
             document.querySelector("h1").innerHTML="Draw!";
